2204a01497e5fe90b3e7b89b562a29e4edbf3e8a
mahamed-gamal <mohammedgamal319mg@gmail.com>
2020-10-24 20:33:46 +0200

    set root view controller

--- PizzaApp/Modules/Home/HomeViewController.swift
+++ PizzaApp/Modules/Home/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  PizzaApp
 //
 //  Created by Mohamed Gamal on 10/24/20.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        
     }
-
-
 }
-
